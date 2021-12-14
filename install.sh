3f14b4ac2d18b7ea8431c3321a96f575be19e08c
Yutaro Hayashi <yutaro50@gmail.com>
2021-12-14 20:47:03 +0900

    大体いい感じになったと思う

--- install.sh
+++ install.sh
@@ -12,11 +12,8 @@ git submodule update
 echo "start setup..."
 for f in .??*; do
     [ "$f" = ".git" ] && continue
-    [ "$f" = ".gitconfig.local.template" ] && continue
-    [ "$f" = ".require_oh-my-zsh" ] && continue
-    [ "$f" = ".gitmodules" ] && continue
 
-    ln -snfv ~/dotfiles/"$f" ~/
+    ln -snfv "$THIS_DIR"/"$f" ~/
 done
 
 cat << END
